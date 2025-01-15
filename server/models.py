43fc66bb7e315e294d61dc44414a36910a0cd9a7
Amene <layla_1380@yahoo.com>
2025-01-15 11:45:56 -0600

    Added password property and setter for password validation and hashing

--- server/models.py
+++ server/models.py
@@ -6,10 +6,12 @@ from sqlalchemy import MetaData, ForeignKey
 from sqlalchemy.orm import validates, relationship
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
+import re
 # Local imports
 from config import db  # This imports the db instance defined in config.py
 
+
+bcrypt = Bcrypt()
 # Models go here!
 
 class PetOwner(db.Model, SerializerMixin):
@@ -18,6 +20,23 @@ class PetOwner(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String, nullable=False, unique=True)
     _hash_password = db.Column(db.String, nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        pattern = re.compile(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*]).{8,}$')
+        if not password or not isinstance(password, str):
+            raise ValueError('Password is required and must be string.')
+        if not pattern.match(password):
+            raise ValueError('Password must be at least 8 characters long. It must include at least 1 lowercase, 1 uppercase letter, and at least 1 (!@#$%^&*)')
+        self._hash_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self._hash_password, password)
+
 
 
 
