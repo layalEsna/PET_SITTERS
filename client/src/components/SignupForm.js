1c70742d9179ce7bc72f10b24d85a640b44cd743
Amene <layla_1380@yahoo.com>
2025-01-16 09:46:47 -0600

    Implement SignupForm UI with form validation, error handling, and navigatio

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -57,8 +57,76 @@ function SignupForm() {
                 .catch((e) => {
                     setErrorMessage(e.message);
                     console.error('Network or server error:', e);
-                });                        
+                })                        
         }
     })
+
+    return (
+        <div>
+
+            <h1>Create an Acount</h1>
+
+
+            {errorMessage && <div className='error'>{errorMessage}</div>}
+
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                    <label htmlFor='user_name'>username</label>
+                    <input
+                        id='user_name'
+                        name='user_name'
+                        type='text'
+                        value={formik.values.user_name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.errors.user_name && formik.touched.user_name && (
+                        <div className='error'>{formik.errors.user_name}</div>
+                    )}
+                </div>
+                <br />
+                <div>
+                    <label htmlFor='password'>password</label>
+                    <input
+                        id='password'
+                        name='password'
+                        type='password'
+                        value={formik.values.password}
+                        onBlur={formik.handleBlur}
+                        onChange={formik.handleChange}
+                        autoComplete='current-password'
+                    />
+                    {formik.errors.password && formik.touched.password && (
+                        <div className='error'>{formik.errors.password}</div>
+                    )}
+                </div>
+                <br />
+                <div>
+                    <label htmlFor='confirm_password'>confirm password</label>
+                    <input
+                        id='confirm_password'
+                        name='confirm_password'
+                        type='password'
+                        value={formik.values.confirm_password}
+                        onBlur={formik.handleBlur}
+                        onChange={formik.handleChange}
+
+                    />
+                    {formik.errors.confirm_password && formik.touched.confirm_password && (
+                        <div className='error'>{formik.errors.confirm_password}</div>
+                    )}
+                </div>
+                <br />
+                <div><button type='submit'>signup</button></div>
+
+            </form>
+            <br />
+            <div>
+                <h3>Already have an account?</h3>
+                <button onClick={()=> navigate('/login')}>login</button>
+            </div>
+        </div>
+    )
+
 }
 export default SignupForm
