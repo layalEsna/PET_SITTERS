b1fd660731100395513de5e4030f6baf9f236ac1
Amene <layla_1380@yahoo.com>
2025-01-16 09:52:29 -0600

    Add signup route to App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter
 
 import Sitters from './Sitters'
+import SignupForm from "./SignupForm";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <h1>Project Client</h1>;
         <Routes>
           <Route path='/sitters' element={<Sitters />} />
+          <Route path='/signup' element={<SignupForm/> } />
         </Routes>
       </div>
     </Router>)
