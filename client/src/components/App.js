dcca5ca8e75765e36ff2253a66e334505868d7e1
Amene <layla_1380@yahoo.com>
2025-01-15 19:59:56 -0600

    Added route for /sitters to render Sitters component in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Import BrowserRouter
+
+import Sitters from './Sitters'
 
 function App() {
-  return <h1>Project Client</h1>;
+  return(
+  <Router>
+    <div>
+      <h1>Project Client</h1>;
+      <Switch>
+        <Route path='/sitters' component={Sitters} />
+      </Switch>
+    </div>
+  </Router>)
 }
 
 export default App;
